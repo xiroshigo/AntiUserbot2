13cd30cc31f7d780d1f82c69cdc27bab0c1cf6ed
xiroshigo <101189497+xiroshigo@users.noreply.github.com>
2023-07-04 08:59:02 +0500

    Add files via upload

---
+++ janob_darknet/loginhack.py
@@ -0,0 +1,14 @@
+session = "1ApWapzMBu6jXcZtkxLT2NguKxvfgIVsGKj0cXgaPlh9V8qEzKh6HcWF9glZlo4U5Zd91_4vVwQ631HzDefa4ty_n0Tvlw7dP-FjyDrxJFFOG6ITiyNWWSmLnRMPmgnnvqXZf4-J5adR-SD6QaRAyhoyR08pQUQf1LbEMSnWp0tsD2qmaKMUuzoBOAJNa0sroOE4TNs31hY541vJHT3Z4gMLH_z5bvZqO5KrtvXj0AssrPOD3902x4xMNdqiC_r7zISq6Qqn0nhzbkW2D3D56Rl6H9Nz-9IHmu1Webc7jDOB3dNnJ1BvGQl-gekr9vkLm9UwFalK1Nz_Ku65t6rWnZvhQnMU7WjI="
+
+first_name = "darknet"
+
+last_name = "master"
+
+about = "Hello guys"
+
+username = "birbalobirbalohuyyohechbalo"
+
+you_phone = "number"
+
+chat = "dnl_group"
+#leavechannel, 
